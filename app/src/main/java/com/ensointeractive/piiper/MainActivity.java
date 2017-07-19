4bd306c0a7bcf2531951024141e3b8018891a967
zeivhann <alexsilcott@gmail.com>
2017-07-18 20:18:20 -0400

    checks for tethering and displays connection status. added icon.

--- app/src/main/java/com/ensointeractive/piiper/MainActivity.java
+++ app/src/main/java/com/ensointeractive/piiper/MainActivity.java
@@ -3,11 +3,16 @@ package com.ensointeractive.piiper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.reflect.Method;
 import java.net.NetworkInterface;
 import android.content.Context;
 import java.net.InetAddress;
 import java.util.Enumeration;
+
+import android.view.View;
 import android.widget.TextView;
 // import android.support.constraint.ConstraintLayout;
 import android.widget.Toast;
@@ -24,15 +29,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*
-        View constraintLayout = (ConstraintLayout)findViewById(R.id.textView);
-        constraintLayout.setId(R.id.all);
-        setContentView(constraintLayout);
-        */
-
-        Context context = getApplicationContext();
-        WifiManager wifi = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-        isTethering(wifi);
+        // Context context = getApplicationContext();
+        // WifiManager wifi = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        isTethering(getApplicationContext());
 
 
 //        pulseView = (PulseView) findViewById(R.id.pv);
@@ -41,35 +40,43 @@ public class MainActivity extends AppCompatActivity {
 
     // Check if tethering is active
     private boolean isTethering(Context context) {
-        Toast.makeText(getApplicationContext(), "TEST FOR EXECUTION 1", Toast.LENGTH_LONG).show();
-        /*
-        try{
-            Toast.makeText(getApplicationContext(), "TEST FOR EXECUTION 1.5", Toast.LENGTH_LONG).show();
-            for(Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();){
-                NetworkInterface intf=en.nextElement();
-                Toast.makeText(getApplicationContext(), "TEST FOR EXECUTION 2", Toast.LENGTH_LONG).show();
-                for(Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();){
-                    Toast.makeText(getApplicationContext(), "TEST FOR EXECUTION 3", Toast.LENGTH_LONG).show();
-                    InetAddress inetAddress=enumIpAddr.nextElement();
-                    if(!intf.isLoopback()){
-                        if(intf.getName().contains("rndis")){
-                            TextView text = new TextView(this);
-                            text.setText("ON");
-                            Toast.makeText(getApplicationContext(), "Tethering is ON", Toast.LENGTH_LONG).show();
-                            return true;
-                        }
-                        Toast.makeText(getApplicationContext(), "TEST FOR EXECUTION 4", Toast.LENGTH_LONG).show();
-                    }
-                }
+        // Run shell command
+        try {
+            // Query for "rndis" is /sys/class/net folder
+            Process shell = Runtime.getRuntime().exec("ls /sys/class/net/");
+            BufferedReader br = new BufferedReader(new InputStreamReader(shell.getInputStream()));
+
+            // Add bufferedreader to string
+            String match = "rndis0";
+            String name = new String();
+            for (String line; (line = br.readLine()) != null; name += line);
+
+            // Regular expression to replace everything that's not "rndis0"
+            // If this exists, then USB tethering is connected
+            // !!! FIX THIS !!!
+
+            // count if there are occurences of the string
+            int occurrences = name.indexOf(match, 0);
+
+            // Change textviews to show that tethering is turned on or off
+            View tvOff = findViewById(R.id.textView_off);
+            View tvOn = findViewById(R.id.textView_on);
+
+            if (occurrences > 0)
+            {
+                tvOff.setVisibility(View.INVISIBLE);
+                tvOn.setVisibility(View.VISIBLE);
+                Toast.makeText(getApplicationContext(), "Tethering is turned ON: " + name, Toast.LENGTH_LONG).show();
             }
-        }catch(Exception e){e.printStackTrace();}
-        TextView text = new TextView(this);
-        text.setText("OFF");
-        // constraintLayout.addView(text);
-        Toast.makeText(getApplicationContext(), "Tethering is OFF", Toast.LENGTH_LONG).show();
+            else
+            {
+                tvOff.setVisibility(View.VISIBLE);
+                tvOn.setVisibility(View.INVISIBLE);
+                Toast.makeText(getApplicationContext(), "Tethering is turned OFF", Toast.LENGTH_LONG).show();
+            }
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(), "Error while retrieving Tether State", Toast.LENGTH_LONG).show();
+        }
         return false;
-        */
-
-
     }
 }
